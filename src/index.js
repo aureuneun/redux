ddabb56daa19ba6a7ddfc160e849b9d42590cee0
aureuneun <aureuneun@gmail.com>
2020-06-25 09:50:19 +0900

    Counter with VanillaJS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const plus = document.getElementById("plus");
+const number = document.getElementById("number");
+const minus = document.getElementById("minus");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let count = 0;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const updateText = () => {
+  number.innerHTML = count;
+};
+
+const onPlus = (e) => {
+  count = count + 1;
+  updateText();
+};
+
+const onMinus = (e) => {
+  count = count - 1;
+  updateText();
+};
+
+const init = () => {
+  updateText();
+  plus.addEventListener("click", onPlus);
+  minus.addEventListener("click", onMinus);
+};
+
+init();
